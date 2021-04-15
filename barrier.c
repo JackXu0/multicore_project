082c44ded0f7e10c04a15bbb35128a04f0b600c4
Zhuocheng Xu <zx1001@access2.cims.nyu.edu>
2021-04-15 14:52:51 -0400

    111

---
+++ barrier.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+int main(int argc, char *argv[])
+{
+    int num_of_threads = atoi(argv[1]);
+
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+
+    printf ("%s \n", "start");
+    int i = 0;
+
+    #pragma omp parallel num_threads(num_of_threads) private(i) 
+    {
+        printf ("start thread %i \n", omp_get_thread_num());
+        #pragma omp for
+        for (i=0; i<4; i++)
+        {
+            printf ("start %i \n", i);
+            unsigned int time_to_sleep = i; // sleep 10 seconds
+            while(time_to_sleep)
+                time_to_sleep = sleep(time_to_sleep);
+            printf ("end %i \n", i);
+        }
+        #pragma omp barrier
+        #pragma omp for
+        for (i=0; i<4; i++)
+        {
+            printf ("start %i \n", i);
+            unsigned int time_to_sleep = 4 - i; // sleep 10 seconds
+            while(time_to_sleep)
+                time_to_sleep = sleep(time_to_sleep);
+            printf ("end %i \n", i);
+        }
+        printf ("end thread %i \n", omp_get_thread_num());
+    }
+
+    gettimeofday(&stop, NULL);
+    printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec); 
+    printf ("%s \n", "end");
+    return 0;
+}
---
+++ unbalanced.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+int main(int argc, char *argv[])
+{
+    int num_of_threads = atoi(argv[1]);
+
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    
+    printf ("%s \n", "start");
+
+    // unsigned int time_to_sleep = 10; // sleep 10 seconds
+    // while(time_to_sleep)
+    //     time_to_sleep = sleep(time_to_sleep);
+    int i;
+
+    #pragma omp parallel for num_threads(num_of_threads)
+    for(i = 0; i < 10; i++){
+        int thread_num = omp_get_thread_num();
+        if(i < 2){
+            unsigned int time_to_sleep = 10; // sleep 10 seconds
+            while(time_to_sleep)
+                time_to_sleep = sleep(time_to_sleep);
+            printf ("Hello from %i \n", thread_num);
+        }else{
+            printf ("Hello from %i \n", thread_num);
+        }
+        
+    }
+
+    gettimeofday(&stop, NULL);
+    printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec); 
+
+    printf ("%s \n", "end");
+    return 0;
+}
