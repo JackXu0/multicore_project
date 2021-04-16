7eaeaad6ade8183f09bbb65ea4ad0fafc0bed89b
Tongxin Xu <tx557@nyu.edu>
2021-04-16 14:02:31 -0400

    Add a flag for perf annotate in the makefile

--- barrier/barrier.c
+++ barrier/barrier.c
@@ -5,6 +5,33 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+void func1(int pid, int i){
+    printf ("\tThread %i: start %i \n", pid, i);
+    if(pid % 2 == 0) {
+        float time_to_sleep = (float)(5 / (pid+1)); // sleep 5 seconds
+        sleep(time_to_sleep);
+        // for (int j = 0; j < 10000; ) {
+        //     j++;
+        //     printf("%d\n", j);
+        // }
+
+    }
+    printf ("\tThread %i: end %i \n", pid, i);
+}
+
+void func2(int pid, int i){
+    printf ("\tThread %i: start %i \n", pid, i);
+    if(pid % 2 == 1) {
+        float time_to_sleep = (float)(5 / (pid+1)); // sleep 5 seconds
+        sleep(time_to_sleep);
+        // for (int j = 0; j < 10000; ) {
+        //     j++;
+        //     printf("%d\n", j);
+        // }
+    }
+    printf ("\tThread %i: end %i \n", pid, i);
+}
+
 int main(int argc, char *argv[])
 {
     int num_of_threads = atoi(argv[1]);
@@ -14,34 +41,27 @@ int main(int argc, char *argv[])
 
     printf ("%s \n", "start");
     int i = 0;
+    int pid;
 
-    #pragma omp parallel num_threads(num_of_threads) private(i) 
+    #pragma omp parallel num_threads(num_of_threads) private(i, pid) 
     {
-        printf ("start thread %i \n", omp_get_thread_num());
-        #pragma omp for
+        pid = omp_get_thread_num();
+        int thread_start_time = omp_get_wtime();
+        printf ("start thread %i \n", pid);
+        #pragma omp for nowait
         for (i=0; i<10; i++)
         {
-            printf ("start %i \n", i);
-            if(i == 0){
-                unsigned int time_to_sleep = 5; // sleep 10 seconds
-                while(time_to_sleep)
-                    time_to_sleep = sleep(time_to_sleep);
-            }
-            printf ("end %i \n", i);
+            func1(pid, i);
         }
+        printf ("Thread %i reach the barrier\n", pid);
         #pragma omp barrier
         #pragma omp for
         for (i=0; i<10; i++)
         {
-            printf ("start %i \n", i);
-            if(i == 9){
-                unsigned int time_to_sleep = 5; // sleep 10 seconds
-                while(time_to_sleep)
-                    time_to_sleep = sleep(time_to_sleep);
-            }
-            printf ("end %i \n", i);
+            func2(pid, i);
         }
         printf ("end thread %i \n", omp_get_thread_num());
+        printf ("!!! Thread %i's total running time: %f !!!\n", pid, omp_get_wtime() - thread_start_time);
     }
 
     gettimeofday(&stop, NULL);
