a047d1c70c6574675e102f59af318cef634a4a44
Tongxin Xu <tx557@nyu.edu>
2021-04-20 21:31:38 -0400

    Change reference code

--- reference/reference.c
+++ reference/reference.c
@@ -5,28 +5,43 @@ int main(int argc, char *argv[])
     int num_of_threads = atoi(argv[1]);
     int size = (argc == 2)? SIZE: atoi(argv[2]);
 
-    struct timeval stop, start;
-    gettimeofday(&start, NULL);
+    int a[size], b[size];
+
+    for (int i = 0; i < size; i++) {
+        a[i] = rand();
+    }
+
+    int batch = size / num_of_threads;
+    int mod = size % num_of_threads;
+
+    int left[num_of_threads];
+    int right[num_of_threads];
+
+    for (int j = 0, temp = 0; j < num_of_threads; j++) {
+        left[j] = temp;
+        temp = (j < mod)? temp + batch + 1: temp + batch;
+        right[j] = temp;
+    }
 
     printf ("%s \n", "start");
 
-    int i;
-    int a[size], b[size];
-
-    for (i = 0; i < size; i++) {
-        a[i] = rand();
-        // printf("%d\n", a[i]);
-    }
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
 
     #pragma omp parallel for num_threads(num_of_threads) schedule(dynamic)
-    for(i = 0; i < size; i++) {
-        b[i] = a[i];
+    {
+        int pid = omp_get_thread_num();
+        #pragma omp for nowait
+        for (int i = left[pid]; i < right[pid]; i++) {
+            b[i] = a[i];
+        }
     }
-
-    checkCorrectness(a, b, size);
 
     gettimeofday(&stop, NULL);
     printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec); 
+
+    checkCorrectness(a, b, size);
+
     printf ("%s \n", "end");
     return 0;
 }
